cb089a13feb99c8bca745cb73c420ee78eca6554
jenstem <jennifer.stemkowski@gmail.com>
2024-07-09 15:08:44 -0400

    Add CreateEmployeees() + PrintEmployees()

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -3,27 +3,40 @@ using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
-    class Program
+
+class Program
+{
+    static List<string> GetEmployees()
     {
-        static void Main(string[] args)
+        List<string> employees = new List<string>();
+        while (true)
         {
-            List<string> employees = new List<string>();
 
-            while (true)
-            {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine() ?? "";
+            Console.WriteLine("Please enter a name: (leave empty to exit): ");
 
-                if (input == "")
-                {
-                    break;
-                }
-                employees.Add(input);
-            }
-            for (int i = 0; i < employees.Count; i++)
+            string input = Console.ReadLine() ?? "";
+
+            if (input == "")
             {
-                Console.WriteLine(employees[i]);
+                break;
             }
+            employees.Add(input);
+        }
+        return employees;
+    }
+
+    static void PrintEmployees(List<string> employees)
+    {
+        for (int i = 0; i < employees.Count; i++)
+        {
+            Console.WriteLine(employees[i]);
         }
     }
+
+    static void Main(string[] args)
+    {
+        List<string> employees = GetEmployees();
+        PrintEmployees(employees);
+    }
+}
 }
