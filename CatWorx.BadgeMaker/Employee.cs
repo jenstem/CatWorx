3b5a6ec8aaf195fb2720c34e441129bad60ad63f
jenstem <jennifer.stemkowski@gmail.com>
2024-07-11 12:07:47 -0400

    Update constructor to include Id + PhotoUrl

--- CatWorx.BadgeMaker/Employee.cs
+++ CatWorx.BadgeMaker/Employee.cs
@@ -4,11 +4,13 @@ namespace CatWorx.BadgeMaker
     {
         public string FirstName;
         public string LastName;
-        // public int Id;
-        // public string PhotoUrl;
-        public Employee(string firstName, string lastName) {
+        public int Id;
+        public string PhotoUrl;
+        public Employee(string firstName, string lastName, int id, string photoUrl) {
             FirstName = firstName;
             LastName = lastName;
+            Id = id;
+            PhotoUrl = photoUrl;
         }
         public string GetFullName() {
             return FirstName + " " + LastName;
