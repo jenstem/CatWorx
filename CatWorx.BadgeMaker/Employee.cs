412fbccef167a826acad2ab20e5818ae2357757e
jenstem <jennifer.stemkowski@gmail.com>
2024-07-11 11:01:47 -0400

    Fix spelling error

--- CatWorx.BadgeMaker/Employee.cs
+++ CatWorx.BadgeMaker/Employee.cs
@@ -4,11 +4,11 @@ namespace CatWorx.BadgeMaker
     {
         public string FirstName;
         public string LastName;
-        public int Id;
-        public string PhotoUrl;
+        // public int Id;
+        // public string PhotoUrl;
         public Employee(string firstName, string lastName) {
             FirstName = firstName;
-            LastName = LastName;
+            LastName = lastName;
         }
         public string GetFullName() {
             return FirstName + " " + LastName;
